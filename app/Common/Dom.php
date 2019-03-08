ddc8446da12beab5a78ee2d48bdd242fc3520808
pk <790378735@qq.com>
2019-03-08 08:41:16 +0800

    add

--- app/Common/Dom.php
+++ app/Common/Dom.php
@@ -4,15 +4,87 @@ namespace App\Common;
 
 class Dom
 {
-    public static function getContent($content,$feature,$tag)
+
+    private $tag;
+    private $content;
+    private $feature;
+    private $end;
+    private $greedy = false;
+
+
+
+
+    public function all()
     {
-
-
-        $preg = "/<{$tag}[\s\w\"']?{$feature}[\s\w\"']?>([\s\S]+)<div class=\"page\">/";
+        $preg = $this->preg();
         var_dump($preg);
-        $res = preg_match_all($preg,$content,$match);
+        preg_match_all($preg,$this->content,$match);
+        $this->setParam();
+        $this->greedy = false;
+        return $match[1];
+    }
 
-        echo $match[1][0];
+    public function row()
+    {
+        return current($this->all());
+    }
+
+
+
+    public function setParam($content = '',$tag = '', $feature = '',$end = '')
+    {
+        $this->setTag($tag);
+        $this->setFeature($feature);
+        $this->setContent($content);
+        $this->setEnd($end);
+        return $this;
+    }
+
+
+    public function preg()
+    {
+        $start  =   $this->feature ? "<{$this->tag}[\s\S]*?{$this->feature}[\s\S]*?>" : "<{$this->tag}[\s\S]*?>";
+        $end    =   $this->end ? $this->end : "<\/{$this->tag}>";
+        $center =   $this->greedy ? "([\s\S]*?)" : "([\s\S]+)";
+        return "/{$start}{$center}{$end}/";
+    }
+
+
+    /**
+     * @param mixed $tag
+     */
+    public function setTag($tag): void
+    {
+        $this->tag = $tag;
+    }
+
+    /**
+     * @param mixed $feature
+     */
+    public function setFeature($feature): void
+    {
+        $this->feature = $feature;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content): void
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @param mixed $end
+     */
+    public function setEnd($end): void
+    {
+        $this->end = $end;
+    }
+
+    public function setGreedy($greedy)
+    {
+        $this->greedy = $greedy;
     }
 
 
--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -27,10 +27,33 @@ class JobController extends Controller
 
     public function getLocal()
     {
-        $boss = file_get_contents('1.txt');
+        $boss = file_get_contents('1.html');
 
-        Dom::getContent($boss,'class="job-list"','div');
 
+        try{
+
+            $dom = new Dom();
+            $div = $dom->setParam($boss,'div','class="job-list"','<div class="page">')->row();
+
+            $dom->setGreedy(true);
+            $li = $dom->setParam($div,'li')->all();
+
+            $salary = [];
+
+            foreach ($li as $v)
+            {
+                $dom->setGreedy(true);
+                $company[] = $dom->setParam($dom->setParam($v,'div','class="company-text"')->row(),'a')->row();
+                $salary[] = $dom->setParam($v,'span','class="red"')->row();
+            }
+
+
+
+
+        }catch(\Exception $e)
+        {
+            var_dump($e->getMessage());
+        }
     }
 
 }
